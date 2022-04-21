c947001c60fcd48057d33ccc1e9754ec6121d068
deserts <int8@foxmail.com>
2022-04-21 09:34:36 +0800

    add functions of button

--- extension.js
+++ extension.js
@@ -25,6 +25,60 @@ var NewActivitiesButton = GObject.registerClass(
 
 			this._xdndTimeOut = 0;
 		}
+		
+		handleDragOver(source, _actor, _x, _y, _time) {
+			if (source != Main.xdndHandler)
+				return DND.DragMotionResult.CONTINUE;
+
+			if (this._xdndTimeOut != 0)
+				GLib.source_remove(this._xdndTimeOut);
+			GLib.Source.set_name_by_id(this._xdndTimeOut, '[gnome-shell] this._xdndToggleOverview');
+
+			return DND.DragMotionResult.CONTINUE;
+		}
+
+		vfunc_captured_event(event) {
+			if (event.type() == Clutter.EventType.BUTTON_PRESS ||
+				event.type() == Clutter.EventType.TOUCH_BEGIN) {
+				if (!Main.overview.shouldToggleByCornerOrButton())
+					return Clutter.EVENT_STOP;
+			}
+			return Clutter.EVENT_PROPAGATE;
+		}
+
+		vfunc_event(event) {
+			if (event.type() == Clutter.EventType.TOUCH_END ||
+				event.type() == Clutter.EventType.BUTTON_RELEASE) {
+				if (Main.overview.shouldToggleByCornerOrButton())
+					Main.overview.toggle();
+			}
+
+			return Clutter.EVENT_PROPAGATE;
+		}
+
+		vfunc_key_release_event(keyEvent) {
+			let symbol = keyEvent.keyval;
+			if (symbol == Clutter.KEY_Return || symbol == Clutter.KEY_space) {
+				if (Main.overview.shouldToggleByCornerOrButton()) {
+					Main.overview.toggle();
+					return Clutter.EVENT_STOP;
+				}
+			}
+
+			return Clutter.EVENT_PROPAGATE;
+		}
+
+		_xdndToggleOverview() {
+			let [x, y] = global.get_pointer();
+			let pickedActor = global.stage.get_actor_at_pos(Clutter.PickMode.REACTIVE, x, y);
+
+			if (pickedActor == this && Main.overview.shouldToggleByCornerOrButton())
+				Main.overview.toggle();
+
+			GLib.source_remove(this._xdndTimeOut);
+			this._xdndTimeOut = 0;
+			return GLib.SOURCE_REMOVE;
+		}
 	}
 );
 
